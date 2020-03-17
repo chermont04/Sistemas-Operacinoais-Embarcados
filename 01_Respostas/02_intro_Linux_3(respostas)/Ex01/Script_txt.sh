e271e5814decbc9be5530f677e076378a56f7db2
Felipe Chermont <47324006+chermont04@users.noreply.github.com>
2020-03-17 19:12:48 -0300

    Add files via upload

---
+++ 01_Respostas/02_intro_Linux_3(respostas)/Ex01/Script_txt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+for i in {1..100} #para i começando em um e indo até cem
+	do
+	echo 'Número do arquivo = '$i > teste$i.txt #Cria um arquivo txt, com o nome teste"i" e conteudo "numero do arquivo = "i"
+	done 
+
