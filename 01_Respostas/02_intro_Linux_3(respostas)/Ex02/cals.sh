e6e91c31f428dff3f11b10ef50b08b800f741dae
Felipe Chermont <47324006+chermont04@users.noreply.github.com>
2020-03-17 19:11:50 -0300

    Adicionando respostas as questões referentes a terceira aula

---
+++ 01_Respostas/02_intro_Linux_3(respostas)/Ex02/cals.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cal $i -m $1
+cal $4 -m $3
+cal $6 -m $5
+cal $8 -m $7
---
+++ 01_Respostas/02_intro_Linux_3(respostas)/Ex04/sites_download.sh
@@ -0,0 +1,6 @@
+
+#!/bin/bash
+wget https://github.com/DiogoCaetanoGarcia/Sistemas_Embarcados/raw/master/Aulas/01_Linux%20b%C3%A1sico.pdf
+wget https://github.com/DiogoCaetanoGarcia/Sistemas_Embarcados/raw/master/Aulas/01_Linux%20b%C3%A1sico_Shell_Script.pdf
+wget https://github.com/DiogoCaetanoGarcia/Sistemas_Embarcados/raw/master/Aulas/01_Sistemas%20Embarcados%20-%20Aula%201%20-%20Introdu%C3%A7%C3%A3o.pdf
+
